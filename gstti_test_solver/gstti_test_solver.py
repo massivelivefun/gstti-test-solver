a0fd1f5d43903849951a7f1b8f17dbafd8661633
massivelivefun <matthewlancefuller@gmail.com>
2021-10-12 21:15:50 -0700

    Revamp main script file

--- gstti_test_solver/gstti_test_solver.py
+++ gstti_test_solver/gstti_test_solver.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from selenium import webdriver
-from typing import Dict, List
+from typing import Dict
 
 import errno
 import os
@@ -36,27 +36,35 @@ tests: Dict[str, TestType] = {
     "renewal10": TestType.RENEWAL_10_HOUR
 }
 
-def eprint(*args, **kwargs):
+def help() -> None:
+    print("Usage:\tgstti_test_solver <username> <password> <test_name>\t")
+    print("\tgstti_test_solver --help")
+
+def eprint(*args, **kwargs) -> None:
     print(*args, file=sys.stderr, **kwargs)
 
 def which_test(test_str: str) -> TestType:
     return tests[test_str] if test_str in tests else TestType.ERROR
 
-def strip_solution(list: list[str]) -> list:
-    stripped_list: List = []
+def create_solution_list(test_name: str) -> list[str]:
+    test_name_path = os.path.join(os.path.dirname(__file__), "solutions", test_name) + ".txt"
+    return strip_solution(open(test_name_path, 'r').readlines())
+
+def strip_solution(list: list[str]) -> list[str]:
+    stripped_list = []
     for element in list:
         stripped_list.append(element.strip())
     return stripped_list
 
-def login(driver, username: str, password: str):
-    usernamefield = driver.find_element_by_id("ctl00_ContentPlaceHolder1_txtEmail")
-    usernamefield.send_keys(username)
-    passwordfield = driver.find_element_by_id("ctl00_ContentPlaceHolder1_txtPassword")
-    passwordfield.send_keys(password)
+def login(driver, username: str, password: str) -> None:
+    username_field = driver.find_element_by_id("ctl00_ContentPlaceHolder1_txtEmail")
+    username_field.send_keys(username)
+    password_field = driver.find_element_by_id("ctl00_ContentPlaceHolder1_txtPassword")
+    password_field.send_keys(password)
     login_button = driver.find_element_by_id("ctl00_ContentPlaceHolder1_cmdSubmit")
     login_button.click()
 
-def navigate_to_test(driver, test_value):
+def navigate_to_test(driver, test_value) -> None:
     match test_value:
         case TestType.RENEWAL_2_HOUR:
             driver.get("https://gstti.com/ExamStart.aspx?EID=230")
@@ -73,65 +81,61 @@ def navigate_to_test(driver, test_value):
         case TestType.LICENSE_43_HOUR:
             driver.get("https://gstti.com/ExamStart.aspx?EID=255")
 
-def next_test_question(driver):
-    button = driver.find_element_by_id("ctl00_ContentPlaceHolder1_cmdNext")
-    button.click()
-
-def select_answer(driver, answer: str):
-    action = "ctl00_ContentPlaceHolder1_optAnswer"
-    match answer:
-        case "A":
-            action += "A"
-        case "B":
-            action += "B"
-        case "C":
-            action += "C"
-        case "D":
-            action += "D"
-        case _:
-            raise ValueError('unrecognized value in solutions list')
-    button = driver.find_element_by_id(action)
-    button.click()
-
-def start_test(driver, solutions: list[str]):
+def complete_test(driver, solutions: list[str]) -> None:
     next_test_question(driver)
     for element in solutions:
         select_answer(driver, element)
         next_test_question(driver)
 
-def solve_test(username: str, password: str, test_name: str, test_value: TestType):
-    # Need to fix path handling
-    # cur_path = os.path.dirname(__file__)
-    # solutions_path = os.path.relpath('./solutions', cur_path)
-    test_name_path = "gstti_test_solver/solutions/" + test_name + ".txt"
-    # print(cur_path)
-    # print(os.getcwd())
-    file = open(test_name_path, 'r')
-    answers = file.readlines()
-    solution_list = strip_solution(answers)
-    
+def next_test_question(driver) -> None:
+    button = driver.find_element_by_id("ctl00_ContentPlaceHolder1_cmdNext")
+    button.click()
+
+def select_answer(driver, answer: str) -> None:
+    answer_id = "ctl00_ContentPlaceHolder1_optAnswer"
+    match answer:
+        case "A":
+            answer_id += "A"
+        case "B":
+            answer_id += "B"
+        case "C":
+            answer_id += "C"
+        case "D":
+            answer_id += "D"
+        case _:
+            raise ValueError("Unrecognized value in solution list.")
+    answer_button = driver.find_element_by_id(answer_id)
+    answer_button.click()
+
+def solve_test(username: str, password: str, test_name: str, test_value: TestType) -> None:
+    solution_list = create_solution_list(test_name)
     driver = webdriver.Firefox()
     driver.get("https://gstti.com")
-    
     login(driver, username, password)
     navigate_to_test(driver, test_value)
-    start_test(driver, solution_list)
-    
+    complete_test(driver, solution_list)
     input("Test Complete: Press Enter to close the window...")
     driver.close()
 
 # gstti_test_solver <username> <password> <test_name>
-
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        test = which_test(sys.argv[3])
-        if test == TestType.ERROR:
-            eprint("Unrecognized <test_name>: probably not a valid test...")
-            eprint("\tgstti_test_solver <username> <password> <test_name>")
-            sys.exit(errno.ENOENT)
-        solve_test(sys.argv[1], sys.argv[2], sys.argv[3], test)
-        sys.exit()
-    else:
-        eprint("This script is expecting to take only four arguments...")
-        eprint("\tgstti_test_solver <username> <password> <file_name>")
-        sys.exit(errno.EPERM)
+    match len(sys.argv):
+        case 2:
+            if sys.argv[1] == "--help":
+                help()
+            else:
+                eprint("Unrecognized secondary flag: " + sys.argv[1])
+                eprint("Only supported script flag is `--help`")
+                sys.exit(errno.EPERM)
+        case 4:
+            test = which_test(sys.argv[3])
+            if test == TestType.ERROR:
+                eprint("Unrecognized <test_name>:" + sys.argv[3] + " probably not a valid gstti test.")
+                eprint("gstti_test_solver <username> <password> <test_name>")
+                sys.exit(errno.ENOENT)
+            solve_test(sys.argv[1], sys.argv[2], sys.argv[3], test)
+            sys.exit()
+        case _:
+            eprint("Unrecognized script usage...")
+            eprint("Please use `gstti_test_solver --help` for more information.")
+            sys.exit(errno.EPERM)
